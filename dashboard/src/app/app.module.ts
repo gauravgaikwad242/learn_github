97c327864c1fd632052baefe7272d4ac8422cb66
Aloke Maity <meet2aloke@gmail.com>
2023-02-22 11:39:43 -0500

    added component research

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResearchComponent } from './components/research/research.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResearchComponent
   ],
   imports: [
     BrowserModule
